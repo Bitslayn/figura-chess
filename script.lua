5a4bb9e4ae9fbb3d3c1741ee4c2ffae0bbccf650
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2024-10-10 05:18:14 -0400

    RE-Update script.lua smh

--- script.lua
+++ script.lua
@@ -3,11 +3,9 @@ MainPage = action_wheel:newPage()
 action_wheel:setPage(MainPage)
 
 local moveBoard = MainPage:newAction()
-:onLeftClick(function()
-  pings.board(
-    math.round(player:getPos().x) * 100,
-    math.round(player:getPos().y) * 100,
-    math.round(player:getPos().z) * 100,
-    0
-  )
-end)
+    :setItem(
+    'player_head[profile={properties:[{value:"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWE5OTA1ZDQzNDBmZTExZmZlNWRlNGFjMjc4MGEwYzM3ZjdmZTI3ZGY5ZjYyMjdiMTlmNjk4MzAwZjdhNzgzZiJ9fX0=",name:textures}]}]')
+    :setTitle("Move chess board")
+    :onLeftClick(function()
+      isMovingBoard = true
+    end)
